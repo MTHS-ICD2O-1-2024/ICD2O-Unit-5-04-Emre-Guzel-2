d7fe2c49cb719d7d9dafefe7191c4afa1ff1ce8b
default <yunus.emre.guzel@mths.ca>
2025-04-29 22:11:41 +0000

    fixed the error

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: April 22 2025
+// Created on: April 29 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 // Setting the functoin 
 function musuemDiscountAgeCalculator() {
   // Setting the variables
-  const age = parseInt(document.getElementById("age").value);
-  const day = document.getElementById("day").value;
-  let message = "";
+  const age = parseInt(document.getElementById("age").value)
+  const day = document.getElementById("day").value
+  let message = ""
 
   // Setting the if statement
-  if ((day === "Tuesday" || day === "Thursday") || (age > 12 && age < 21)) {
+  if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21)){
     message = "You're eligible for student pricing";
   } else {
-    message = "You must pay the regular pricing";
+    message = "You must pay the regular pricing"
   }
 
-  document.getElementById("result").innerHTML = message;
+  document.getElementById("result").innerHTML = message
 }
